a776fb0b15474dfbe6bc4a64f8d99022c2c59ad3
RubDev666 <125171500+RubDev666@users.noreply.github.com>
2024-12-06 16:03:19 -0600

    metadata improved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Providers } from "./providers";
 import { Header, GridLayout } from "../components/layouts";
 
@@ -7,7 +7,27 @@ import '../styles/index.scss';
 export const metadata: Metadata = {
     title: "Group 4",
     description: "Explore, share, and express what you want with everyone",
+    icons: [
+        {rel: 'icon', url: '/icons/favicon-16x16.png', type: 'image/svg+xml'},
+        {rel: 'icon', url: '/icons/favicon-32x32.png', type: 'image/svg+xml'},
+        {rel: 'icon', url: '/icons/apple-touch-icon.png', type: 'image/svg+xml'},
+    ]
 }; 
+
+/*export const viewport: Viewport = {
+    themeColor: '#070F2B'
+}
+*/
+/*
+            <Head>
+                <meta name="mobile-web-app-capable" content="yes" />
+                <meta name="mobile-web-app-status-bar-style" content="#070F2B" />
+                <meta name="theme-color" content="#070F2B" />
+                <link rel="icon" type="image/svg+xml" href="/icons/favicon-16x16.png" />
+                <link rel="icon" type="image/svg+xml" href="/icons/favicon-32x32.png" />
+                <link rel="icon" type="image/svg+xml" href="/icons/apple-touch-icon.png" />
+            </Head>
+*/
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     //suppressHydrationWarning - remove error in the console
